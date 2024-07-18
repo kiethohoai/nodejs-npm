81cd0b2ce746ec242b05dfea5fc139f3f3a23421
kiethohoai <kietho.060193@gmail.com>
2024-07-18 08:37:40 +0700

    009 Routing

--- index.js
+++ index.js
@@ -1,8 +1,23 @@
 const fs = require('fs');
 const http = require('http');
+const url = require('url');
 
 // TODO SERVER
 const server = http.createServer((req, res) => {
+  const pathName = req.url;
+
+  if (pathName === '/' || pathName === '/overview') {
+    res.end('This is OVERVIEW');
+  } else if (pathName === '/product') {
+    res.end('This is PRODUCT');
+  } else {
+    res.writeHead(404, {
+      'Content-type': 'text/html',
+      'my-own-header': 'hello world',
+    });
+    res.end('<h1>Page not found!</h1>');
+  }
+  //
   res.end('Hello from the server!');
 });
 
