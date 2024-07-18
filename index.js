280c0527e9ff9341367943f22e8f3888296900c1
kiethohoai <kietho.060193@gmail.com>
2024-07-18 16:18:36 +0700

    019 Setting up Prettier in VS Code

--- index.js
+++ index.js
@@ -5,11 +5,24 @@ const url = require('url');
 const slugify = require('slugify');
 
 // TODO SERVER
-const tempOverview = fs.readFileSync(`${__dirname}/templates/template-overview.html`, 'utf-8');
-const tempCart = fs.readFileSync(`${__dirname}/templates/template.cart.html`, 'utf-8');
-const tempProduct = fs.readFileSync(`${__dirname}/templates/template-product.html`, 'utf-8');
+const tempOverview = fs.readFileSync(
+  `${__dirname}/templates/template-overview.html`,
+  'utf-8',
+);
+const tempCart = fs.readFileSync(
+  `${__dirname}/templates/template.cart.html`,
+  'utf-8',
+);
+const tempProduct = fs.readFileSync(
+  `${__dirname}/templates/template-product.html`,
+  'utf-8',
+);
 
-console.log(slugify('My name is David Ho', { lower: true }));
+console.log(
+  slugify('My name is David Ho', {
+    lower: true,
+  }),
+);
 
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
 const dataObj = JSON.parse(data);
@@ -19,7 +32,9 @@ const server = http.createServer((req, res) => {
 
   // todo Overview page
   if (pathname === '/' || pathname === '/overview') {
-    res.writeHead(200, { 'Content-type': 'text/html' });
+    res.writeHead(200, {
+      'Content-type': 'text/html',
+    });
 
     const cartHtml = dataObj.map((el) => replaceTemplate(tempCart, el)).join();
     const output = tempOverview.replace('{%PRODUCT_CART%}', cartHtml);
@@ -27,14 +42,18 @@ const server = http.createServer((req, res) => {
 
     // todo Product page
   } else if (pathname === '/product') {
-    res.writeHead(200, { 'Content-type': 'text/html' });
+    res.writeHead(200, {
+      'Content-type': 'text/html',
+    });
     const product = dataObj[query.id];
     const output = replaceTemplate(tempProduct, product);
     res.end(output);
 
     // todo API
   } else if (pathname === '/api') {
-    res.writeHead(200, { 'Content-type': 'application/json' });
+    res.writeHead(200, {
+      'Content-type': 'application/json',
+    });
     res.end(data);
     // todo Not found page
   } else {
@@ -49,5 +68,5 @@ const server = http.createServer((req, res) => {
 });
 
 server.listen(8000, '127.0.0.1', () => {
-  console.log('Listening to requests on port 8000');
+  console.log('App listening to requests on port 8000');
 });
