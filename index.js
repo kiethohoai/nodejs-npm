20e031f59b2819f79a1a221314efa637352de6c1
kiethohoai <kietho.060193@gmail.com>
2024-07-18 13:45:27 +0700

    012 HTML Templating Filling the Templates

--- index.js
+++ index.js
@@ -3,19 +3,48 @@ const http = require('http');
 const url = require('url');
 
 // TODO SERVER
+const replaceTemplate = (temp, product) => {
+  let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
+  output = output.replace(/{%IMAGE%}/g, product.image);
+  output = output.replace(/{%PRICE%}/g, product.price);
+  output = output.replace(/{%FROM%}/g, product.from);
+  output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
+  output = output.replace(/{%QUANTITY%}/g, product.quantity);
+  output = output.replace(/{%DESCRIPTION%}/g, product.description);
+  output = output.replace(/{%ID%}/g, product.id);
+
+  if (!product.organic) {
+    output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic');
+  }
+  return output;
+};
+
+const tempOverview = fs.readFileSync(`${__dirname}/templates/template-overview.html`, 'utf-8');
+const tempCart = fs.readFileSync(`${__dirname}/templates/template.cart.html`, 'utf-8');
+const tempProduct = fs.readFileSync(`${__dirname}/templates/template-product.html`, 'utf-8');
+
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
 const dataObj = JSON.parse(data);
 
 const server = http.createServer((req, res) => {
   const pathName = req.url;
 
+  // todo Overview page
   if (pathName === '/' || pathName === '/overview') {
-    res.end('This is OVERVIEW');
+    res.writeHead(200, { 'Content-type': 'text/html' });
+
+    const cartHtml = dataObj.map((el) => replaceTemplate(tempCart, el)).join();
+    const output = tempOverview.replace('{%PRODUCT_CART%}', cartHtml);
+    res.end(output);
+
+    // todo Product page
   } else if (pathName === '/product') {
     res.end('This is PRODUCT');
+    // todo API
   } else if (pathName === '/api') {
     res.writeHead(200, { 'Content-type': 'application/json' });
     res.end(data);
+    // todo Not found page
   } else {
     res.writeHead(404, {
       'Content-type': 'text/html',
